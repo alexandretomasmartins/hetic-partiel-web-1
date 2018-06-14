62eaeccdaf68034d8fd10ce783658c0689c1fb96
alexandretomasmartins <alexandre.martins@hetic.net>
2018-06-14 12:47:31 +0200

    Fixes #2

--- main.js
+++ main.js
@@ -14,3 +14,15 @@ container.addEventListener('click', function() {
 change.addEventListener('click', function() {
 	mobilenav.style.display = 'none';
 });
+
+var scroll = window.scrollY;
+var header = document.getElementById("navbar");
+
+var toggle = function() {
+  header.classList.toggle('navbar--hidden', scroll < scrollY);
+  scroll = scrollY;
+}
+
+window.onscroll = toggle;
+window.onload = toggle;
+window.onrisize = toggle;
